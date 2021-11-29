ec95f566fc20d63ca93234c1e5efad2e04264741
redcowe <joshuacowell@hotmail.com>
2021-11-28 23:08:48 -0500

    Added javascript to control the size of the game board

--- script.js
+++ script.js
@@ -1,19 +1,74 @@
-const gameBoard = document.getElementById("gameBoard");
+const gameBoard = document.getElementById('gameBoard');
 
-const gameGrid = document.getElementById("gameGrid");
+const gridSizer = document.getElementById('gridSizer');
 
-const clone = gameGrid.cloneNode(true);
+let multiplier = gridSizer.value;
 
-gameBoard.appendChild(clone);
-gameBoard.appendChild(clone);
-gameBoard.appendChild(clone);
+let total = multiplier * multiplier;
 
-let multiplier = 9;
+let gridPiece = document.createElement('div');
 
 
+function createGrid() {
+
+    for (i = 0; i < total; i++) {
+    
+    let gridPiece = document.createElement('div');
+
+    gridPiece.className = "game-grid";
+
+    gameBoard.appendChild(gridPiece);
+
+    gameBoard.setAttribute("style", `grid-template-columns: repeat(${multiplier}, 1fr); grid-template-rows: repeat(${multiplier}, 1fr);`);
+}};
+
+
+
+function clearGrid() {
+    
+    let child = gameBoard.lastChild;
+
+    while (child) {
+        gameBoard.removeChild(child);
+        child = gameBoard.lastChild;
+    };
+
+};
+
+function getDimensions() {
+
+    multiplier = gridSizer.value;
+
+    total = multiplier ** 2
+};
+
+function updateGrid() {
+
+    clearGrid();
+
+    getDimensions();
+
+    createGrid();
+    
+};
+
+function updateMultiplier() {
+    const sizeMultiplier = document.getElementById('gridSizerValue');
+
+    sizeMultiplier.textContent = `${multiplier}x${multiplier}`
+};
+
+gridSizer.addEventListener('change', function() {
+    updateGrid();
+    updateMultiplier();
+})
+
+createGrid();
 
 
 
 
 
-gameBoard.setAttribute("style", `grid-template-columns: repeat(${multiplier}, 1fr); grid-template-rows: repeat(${multiplier}, 1fr);`);
+
+
+
