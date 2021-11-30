7346ae1f65391769c0892a7dc1219dc829074364
redcowe <joshuacowell@hotmail.com>
2021-11-29 20:47:28 -0500

    Finished the version 1.0 of etch-a-sketch!

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+
+/// Initiallizing variables
+
 const gameBoard = document.getElementById('gameBoard');
 
 const gridSizer = document.getElementById('gridSizer');
+
+const colorPicker = document.getElementById('colorPicker');
+
+const clearButton = document.getElementById('clearButton');
+
+const rainbowButton = document.getElementById('rainbowButton');
+
+const rainbow = ['#9400D3', '#4B0082', '#0000FF' , '#00FF00', '#FFFF00', '#FF7F00', '#FF0000']
+
+let random = Math.floor(Math.random() * rainbow.length);
 
 let multiplier = gridSizer.value;
 
@@ -8,6 +21,14 @@ let total = multiplier * multiplier;
 
 let gridPiece = document.createElement('div');
 
+let color = colorPicker.value;
+
+let clicked = false;
+
+
+
+
+/// Creating the grid
 
 function createGrid() {
 
@@ -17,14 +38,32 @@ function createGrid() {
 
     gridPiece.className = "game-grid";
 
+    gridPiece.setAttribute('id', 'gameGrid');
+
     gameBoard.appendChild(gridPiece);
 
-    gameBoard.setAttribute("style", `grid-template-columns: repeat(${multiplier}, 1fr); grid-template-rows: repeat(${multiplier}, 1fr);`);
+    gameBoard.setAttribute("style", `grid-template-columns: repeat(${multiplier}, 1fr); grid-template-rows: repeat(${multiplier}, 1fr);`)
+
+    setColor()
+
+    
+    gridPiece.addEventListener('mouseover', function () {
+
+        if (clicked == true) {
+            let random = Math.floor(Math.random() * rainbow.length);
+            color = rainbow[random];
+            gridPiece.style.backgroundColor = `${color}`;
+        } else {
+            setColor();
+            gridPiece.style.backgroundColor = `${color}`;
+        };
+        
+    });
 }};
 
+///Resetting the grid
 
-
-function clearGrid() {
+function resetGrid() {
     
     let child = gameBoard.lastChild;
 
@@ -35,6 +74,8 @@ function clearGrid() {
 
 };
 
+///Getting dimensions to update the grid
+
 function getDimensions() {
 
     multiplier = gridSizer.value;
@@ -42,9 +83,11 @@ function getDimensions() {
     total = multiplier ** 2
 };
 
+/// Updating the grid
+
 function updateGrid() {
 
-    clearGrid();
+    resetGrid();
 
     getDimensions();
 
@@ -52,23 +95,40 @@ function updateGrid() {
     
 };
 
+/// Sets color of the grid
+
+function setColor() {
+
+    colorPicker.addEventListener('change', function() {
+        
+        clicked = false;
+
+        color = colorPicker.value;
+    })     
+}
+
+/// Updates the multiplier
+
 function updateMultiplier() {
     const sizeMultiplier = document.getElementById('gridSizerValue');
 
     sizeMultiplier.textContent = `${multiplier}x${multiplier}`
 };
 
+
 gridSizer.addEventListener('change', function() {
     updateGrid();
     updateMultiplier();
 })
 
+clearButton.addEventListener('click', () => {
+    updateGrid();
+})
+
+rainbowButton.addEventListener('click', () => {
+    clicked = true;
+});
+
 createGrid();
-
-
-
-
-
-
 
 
